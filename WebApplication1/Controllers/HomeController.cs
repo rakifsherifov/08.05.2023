b49fb7d37067586b1cf4c86f8e675fb049e1afb2
Rakif R. Sharifov <rakifrsh@code.local>
2023-05-08 12:31:58 +0400

    elave edildi

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly List<Brand> _brand;
+
+        public HomeController()
+        {
+            _brand = new List<Brand>
+            {
+                new Brand { Id =1, Name="Bmw"},
+                new Brand { Id =2, Name="Kia"},
+                new Brand { Id =3, Name="Mercedes"},
+                new Brand { Id =4, Name="Hyundai"},
+                new Brand { Id =5, Name="Lexus"},
+                new Brand { Id =6, Name="Tayota"},
+
+            };
+        }
+        public IActionResult Index()
+        {
+            return View(_brand);
+        }
+    }
+}
---
+++ WebApplication1/Models/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int BrandId { get; set;}
+    }
+}
